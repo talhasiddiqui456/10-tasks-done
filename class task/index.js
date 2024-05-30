f3244cdcce1288868d4dc286692f88c017f9f12d
talhasiddiqui456 <165644985+talhasiddiqui456@users.noreply.github.com>
2024-05-30 19:15:20 +0500

    Add files via upload

---
+++ class task/index.js
@@ -0,0 +1,34 @@
+"use strict";
+// Question NO 2
+//let personName="Talha";
+//console.log('hello',personName,"Would you like to learn some typescript today");
+//Question No 3
+//let personName="Talha";
+//console.log(personName.toLowerCase());
+//console.log(personName.toUpperCase());
+//console.log(personName.charAt(0).toUpperCase()+personName.slice(1).toLowerCase());
+//Question No 4
+//console.log("Audrey Hyburn once said",'"Word IMPOSSIBLE itself says Iam possible"');
+//Question No 5
+//let famousPerson="Audrey Hypburn";
+//let message="oncesaidWord IMPOSSIBLE itself says Iam possible"
+//console.log(famousPerson,message);
+//Question NO 6
+//let whiteSpace="\n\t Talha\t\n"
+//console.log(whiteSpace);
+//let withoutwhiteSpace=whiteSpace.trim();
+//console.log(withoutwhiteSpace);
+//Question No 7 or 8
+//console.log(4+4);
+//console.log(10-2);
+//console.log(2*4);
+//console.log(16/7);
+//Question NO 9
+//let favouriteNumber=7
+//console.log("my favourte number is", favouriteNumber);
+//Question NO 10
+//date: 29-5-24
+//let whiteSpace = "\n\t TALHA\T\N";// \n is used for new line and \t is used for whiteSpace
+//console.log(whiteSpace);
+//let withoutwhiteSpace = whiteSpace.trim()
+//console.log(withoutwhiteSpace);
